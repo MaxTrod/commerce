e4e5368ecc738b6f4864ff114bd5a174dd89ffd1
MaxTrod <maximsiminko@gmail.com>
2025-04-07 18:16:57 +0500

    сделал нормальный слайдер со стрелками без пагинации

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,4 +2,3 @@ import './components/burger-menu.js';
 import './components/animate.js';
 import './components/drop.js';
 import './components/sliders.js';
-import './components/cursor.js';
--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,6 +1,78 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
-// import { cursor } from './cursor.js';
+
+function customCursor() {
+  const cursor = document.querySelector('.case__cursor');
+  const slider = document.querySelector('.case__slider');
+  const slide = document.querySelector('.case__slide');
+  const slideHeight = slide.offsetHeight;
+  const contentHeight = document.querySelector('.case-item__content').offsetHeight;
+  const articleGap = parseInt(window.getComputedStyle(document.querySelector('.case-item')).gap);
+  const btnNext = document.querySelector('.case__next');
+  const btnPrev = document.querySelector('.case__prev');
+  const viewLink = document.querySelector('.case-item__view-link');
+
+  viewLink.style.pointerEvents = `auto`;
+  btnNext.style.height = `calc(${slideHeight}px - ${contentHeight}px - ${articleGap}px)`;
+  btnPrev.style.height = `calc(${slideHeight}px - ${contentHeight}px - ${articleGap}px)`;
+
+  slider.addEventListener("mousemove", function(e) {
+    const targetElement = e.target;
+
+    if(targetElement.closest('.case__next')) {
+      const posX = e.clientX;
+      const posY = e.clientY;
+      cursor.style.left = `${posX}px`;
+      cursor.style.top = `${posY}px`;
+      cursor.classList.add('arrow-right');
+    } else {
+      cursor.classList.remove('arrow-right');
+    }
+
+    if(targetElement.closest('.case__prev')) {
+      const posX = e.clientX;
+      const posY = e.clientY;
+      cursor.style.left = `${posX}px`;
+      cursor.style.top = `${posY}px`;
+      cursor.classList.add('arrow-left');
+    } else {
+      cursor.classList.remove('arrow-left');
+    }
+
+    if(targetElement.closest('.case-item__view-link')) {
+      console.log('я тут')
+      const viewLink = targetElement.closest('.case-item__view-link');
+      const posX = e.clientX;
+      const posY = e.clientY;
+      cursor.style.left = `${posX}px`;
+      cursor.style.top = `${posY}px`;
+      viewLink.style.pointerEvents = `auto`;
+      cursor.classList.add('view-case');
+    } else {
+      cursor.classList.remove('view-case');
+    }
+  })
+
+  slider.addEventListener('mouseenter', function (e) {
+    cursor.classList.add('visible');
+  })
+  slider.addEventListener('mouseleave', function (e) {
+    cursor.classList.remove('visible');
+  })
+  slider.addEventListener('mousedown', function (e) {
+    if(e.target.closest('.case__next') || e.target.closest('.case__prev')) {
+      cursor.classList.add('active-btn');
+    }
+    if(e.target.closest('.case-item__view-link')) {
+      cursor.classList.add('active-link');
+    }
+  })
+  slider.addEventListener('mouseup', function (e) {
+    cursor.classList.remove('active-btn');
+    cursor.classList.remove('active-link');
+  })
+
+}
 
 
 const caseSlider = new Swiper('.case__slider', {
@@ -20,68 +92,18 @@ const caseSlider = new Swiper('.case__slider', {
   },
   on: {
     init: function () {
-    const cursorElem = document.querySelector('[data-cursor]');
-    const imgBlock = document.querySelector('.case-item__image-block');
-    const btnNext = document.querySelector('.case__next');
-    const btnPrev = document.querySelector('.case__prev');
-
-
-    imgBlock.addEventListener("mousemove", function (e) {
-      const posX = e.clientX;
-      const posY = e.clientY;
-      // cursorElem.style.left = `${posX}px`;
-      // cursorElem.style.top = `${posY}px`;
-      cursorElem.style.left = `calc(${posX}px - 59px)`;
-      cursorElem.style.top = `calc(${posY}px - 59px)`;
-    })
-    imgBlock.addEventListener("mouseenter", function (e) {
-      cursorElem.style.opacity = `1`;
-      cursorElem.style.visibility = `visible`;
-    })
-    imgBlock.addEventListener("mouseleave", function (e) {
-      cursorElem.style.opacity = `0`;
-      cursorElem.style.visibility = `hidden`;
-    })
-
-    btnNext.addEventListener("mousemove", function (e) {
-      const posX = e.clientX;
-      const posY = e.clientY;
-      // cursorElem.style.left = `${posX}px`;
-      // cursorElem.style.top = `${posY}px`;
-      cursorElem.style.left = `calc(${posX}px - 59px)`;
-      cursorElem.style.top = `calc(${posY}px - 59px)`;
-    })
-    btnNext.addEventListener("mouseenter", function (e) {
-      cursorElem.style.opacity = `1`;
-      cursorElem.style.visibility = `visible`;
-    })
-    btnNext.addEventListener("mouseleave", function (e) {
-      cursorElem.style.opacity = `0`;
-      cursorElem.style.visibility = `hidden`;
-    })
-
-    btnPrev.addEventListener("mousemove", function (e) {
-      const posX = e.clientX;
-      const posY = e.clientY;
-      // cursorElem.style.left = `${posX}px`;
-      // cursorElem.style.top = `${posY}px`;
-      cursorElem.style.left = `calc(${posX}px - 59px)`;
-      cursorElem.style.top = `calc(${posY}px - 59px)`;
-    })
-    btnPrev.addEventListener("mouseenter", function (e) {
-      cursorElem.style.opacity = `1`;
-      cursorElem.style.visibility = `visible`;
-      cursorElem.classList.add('rotate-arrow');
-    })
-    btnPrev.addEventListener("mouseleave", function (e) {
-      cursorElem.style.opacity = `0`;
-      cursorElem.style.visibility = `hidden`;
-      cursorElem.classList.remove('rotate-arrow');
-    })
-
+      customCursor();
     },
 
     slideChange: function () {
+      const swiperSlides = document.getElementsByClassName('swiper-slide');
+      for (let index = 0; index < swiperSlides.length; index++) {
+        const element = swiperSlides[index];
+        const viewLink = element.querySelector('.case-item__view-link');
+        viewLink.style.pointerEvents = `none`;
+        }
+        const viewLinkActive = swiperSlides[caseSlider.realIndex].querySelector('.case-item__view-link');
+        viewLinkActive.style.pointerEvents = `auto`;
     },
   }
 });
@@ -89,38 +111,48 @@ const caseSlider = new Swiper('.case__slider', {
 
 
 
-const swiperSlides = document.getElementsByClassName('swiper-slide')
 
-caseSlider.on('slideChange', function() {
-  const otherSlides = swiperSlides
-  for (let index = 0; index < swiperSlides.length; index++) {
-    const element = swiperSlides[index];
-    const imgBlock = element.querySelector('.case-item__image-block');
-    const cursorElem = document.querySelector('[data-cursor]');
 
-    imgBlock.addEventListener("mouseenter", function (e) {
-      cursorElem.style.opacity = `0`;
-      cursorElem.style.visibility = `hidden`;
-    })
-  }
 
-  const imgBlockActive = swiperSlides[caseSlider.realIndex].querySelector('.case-item__image-block');
-  const cursorElem = document.querySelector('[data-cursor]');
-  imgBlockActive.addEventListener("mousemove", function (e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-    cursorElem.style.left = `calc(${posX}px - 59px)`;
-    cursorElem.style.top = `calc(${posY}px - 59px)`;
-  })
-  imgBlockActive.addEventListener("mouseenter", function (e) {
-    cursorElem.style.opacity = `1`;
-    cursorElem.style.visibility = `visible`;
-  })
-  imgBlockActive.addEventListener("mouseleave", function (e) {
-    cursorElem.style.opacity = `0`;
-    cursorElem.style.visibility = `hidden`;
-  })
-});
+
+// const swiperSlides = document.getElementsByClassName('swiper-slide');
+
+// caseSlider.on('slideChange', function() {
+//   const otherSlides = swiperSlides
+//   for (let index = 0; index < swiperSlides.length; index++) {
+//     const element = swiperSlides[index];
+//     const viewLink = element.querySelector('.case-item__view-link');
+//     const cursorElem = document.querySelector('[data-cursor]');
+
+//     viewLink.addEventListener("mouseenter", function (e) {
+//       cursorElem.style.opacity = `0`;
+//       cursorElem.style.visibility = `hidden`;
+//       cursorElem.classList.remove('rotate-arrow');
+//     })
+//   }
+
+//   const viewLinkActive = swiperSlides[caseSlider.realIndex].querySelector('.case-item__view-link');
+//   const cursorElem = document.querySelector('[data-cursor]');
+
+//   viewLinkActive.addEventListener("mousemove", function (e) {
+//     const cursorElemViewHeight = cursorElem.offsetHeight;
+//     const cursorElemViewWidth = cursorElem.offsetWidth;
+//     const posX = e.clientX;
+//     const posY = e.clientY;
+//     cursorElem.style.left = `calc(${posX}px - ${cursorElemViewWidth / 2}px)`;
+//     cursorElem.style.top = `calc(${posY}px - ${cursorElemViewHeight / 2}px)`;
+//   })
+//   viewLinkActive.addEventListener("mouseenter", function (e) {
+//     cursorElem.style.opacity = `1`;
+//     cursorElem.style.visibility = `visible`;
+//     cursorElem.classList.add('view-case');
+//   })
+//   viewLinkActive.addEventListener("mouseleave", function (e) {
+//     cursorElem.style.opacity = `0`;
+//     cursorElem.style.visibility = `hidden`;
+//     cursorElem.classList.remove('view-case');
+//   })
+// });
 
 
 
